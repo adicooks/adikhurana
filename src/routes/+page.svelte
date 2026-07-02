d4d129d0098f1d13e0d1cf24da1b48ef06ca2268
adicooks <109033004+adicooks@users.noreply.github.com>
2026-07-02 17:57:16 -0400

    Fix compact homepage spacing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -115,7 +115,7 @@
   }
 </script>
 
-<main class="h-screen fixed w-screen overflow-hidden flex flex-col">
+<main class="h-screen fixed w-screen overflow-hidden flex flex-col pt-12">
   <div class="flex flex-col justify-start items-start m-auto max-w-7xl relative w-full">
     <div class="hidden md:block absolute top-0 bottom-0 right-0 w-14 bg-gradient-to-r from-transparent to-[#EEEEEE] z-10" />
     <div class="hidden md:block absolute top-0 bottom-0 left-0 w-14 bg-gradient-to-l from-transparent to-[#EEEEEE] z-10" />
@@ -205,7 +205,7 @@
 
   <div class="w-full flex flex-col sm:mb-0 relative fade-in-bottom">
     <button
-      class="z-10 absolute transition-all shadow-md hover:shadow-lg rounded-full flex items-center justify-center ring-1 hover:ring-4 ring-[#C5C7CA] hover:ring-blue-400 bg-[#EEEEEE] w-10 h-10 sm:h-20 sm:w-20 top-2.5 left-2.5 sm:top-6 sm:left-6"
+      class="z-10 absolute transition-all shadow-md hover:shadow-lg rounded-full flex items-center justify-center ring-1 hover:ring-4 ring-[#C5C7CA] hover:ring-blue-400 bg-[#EEEEEE] w-10 h-10 sm:h-20 sm:w-20 top-9 left-2.5 sm:top-6 sm:left-6"
       on:click={playMusic}
     >
       <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none"
@@ -220,7 +220,7 @@
 
     <a
       href="mailto:theadikhurana@gmail.com"
-      class="z-10 absolute transition-all shadow-md hover:shadow-lg rounded-full flex ring-1 hover:ring-4 ring-[#C5C7CA] hover:ring-blue-400 bg-[#EEEEEE] w-10 h-10 sm:h-20 sm:w-20 top-2.5 right-2.5 sm:top-6 sm:right-6"
+      class="z-10 absolute transition-all shadow-md hover:shadow-lg rounded-full flex ring-1 hover:ring-4 ring-[#C5C7CA] hover:ring-blue-400 bg-[#EEEEEE] w-10 h-10 sm:h-20 sm:w-20 top-9 right-2.5 sm:top-6 sm:right-6"
     >
       <img class="m-auto" src={Email} alt="Email" />
     </a>
