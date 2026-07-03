2b72830f4688c14f4c0709f3848b336b26dfd75b
adicooks <109033004+adicooks@users.noreply.github.com>
2026-07-02 20:32:49 -0400

    fix github

--- src/lib/components/cards/GithubCard.svelte
+++ src/lib/components/cards/GithubCard.svelte
@@ -3,13 +3,14 @@
   import Github from "$lib/assets/github.svg";
 
   export let text = "pip install code";
+  export let photo = "";
 </script>
 
 <a href="https://github.com/adicooks" target="_blank" rel="noopener noreferrer">
   <Card additionalClasses="hover:scale-100 bg-[#010313] cursor-pointer">
     <div class="px-[22px] py-4 flex flex-col justify-end items-start text-white h-full">
       <img
-        class="relative z-10 translate-y-6 group-hover:translate-y-0 transition-transform"
+        class="translate-y-6 group-hover:translate-y-0 transition-transform"
         src={Github}
         width="30"
         height="30"
@@ -17,7 +18,7 @@
         decoding="async"
       />
       <span
-        class="relative z-10 translate-y-6 group-hover:translate-y-0 transition-transform text-xl mt-1 font-semibold"
+        class="translate-y-6 group-hover:translate-y-0 transition-transform text-xl mt-1 font-semibold"
         >adicooks.git</span
       >
       <p
@@ -25,14 +26,16 @@
       >
         {text}
       </p>
-      <img
-        class="absolute -right-14 top-5 z-0 h-[84px] w-[638px] max-w-none opacity-65 transition-transform group-hover:-translate-y-2"
-        src="/api/github-contributions.svg"
-        alt=""
-        width="686"
-        height="88"
-        decoding="async"
-      />
+      {#if photo}
+        <img
+          class="absolute top-5 left-20 w-[256px] h-[158px] group-hover:-translate-y-2 transition-transform opacity-60"
+          src={photo}
+          alt="Background"
+          width="256"
+          height="158"
+          decoding="async"
+        />
+      {/if}
     </div>
   </Card>
 </a>
